9394cc7af2554643507c58b050fea996cd8780fb
GettiGrinn <71683000+GettiGrinn@users.noreply.github.com>
2020-10-20 10:56:26 +0600

    Update choice_buttons.py

--- choice_buttons.py
+++ choice_buttons.py
@@ -11,7 +11,9 @@ user_menu = ReplyKeyboardMarkup(
         [
             KeyboardButton(text="Индекс и капитализация"),
             KeyboardButton(text="Ценные бумаги"),
-
+        ],
+        [
+            KeyboardButton(text="Уведомления")
         ],
     ],
     resize_keyboard=True
@@ -21,5 +23,16 @@ trade_button = InlineKeyboardMarkup(inline_keyboard=[
     [
         InlineKeyboardButton('\U0001F4CE Посмотреть торги по компаниям', callback_data='company')
 
+    ]
+])
+
+notification_button = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton('Ценные бумаги', callback_data='notification_securities')
+
+
+    ],
+    [
+        InlineKeyboardButton('Отключить уведомления', callback_data='notification_disable')
     ]
 ])
