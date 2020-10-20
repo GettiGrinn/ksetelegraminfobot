469c17269409caad312c1070c8caf8719ee5f57b
GettiGrinn <71683000+GettiGrinn@users.noreply.github.com>
2020-10-20 10:57:37 +0600

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -95,14 +95,14 @@ def get_listing_result(query_text):
         return get_listing("ОАО \"РСК Банк\"")
     elif query_text == "MAIR":
         return get_listing("ОАО \"Международный аэропорт Манас\"")
-    elif query_text == "AYLB":
-        return get_listing("ОАО \"Айыл Банк\"")
     elif query_text == "FKDB":
         return get_listing("ОАО \"ФинансКредитБанк КАБ\"")
+    elif query_text == "AYLB":
+        return get_listing("ОАО \"Айыл Банк\"")
     elif query_text == "ELST":
         return get_listing("ОАО Электрические станции")
     elif query_text == "KELD":
-        return  get_listing("ОАО Келдике")
+        return get_listing("ОАО Келдике")
     elif query_text == "SELK":
         return get_listing("ОАО Северэлектро")
     elif query_text == "UCHK":
@@ -116,7 +116,7 @@ def get_listing_result(query_text):
     elif query_text == "CHGS":
         return get_listing("ОАО Чакан ГЭС")
     elif query_text == "EHSUb":
-        return  get_listing("ОсОО \"ИХСАН-ОРИКС\"")
+        return get_listing("ОсОО \"ИХСАН-ОРИКС\"")
     elif query_text == "TEPL":
         return get_listing("ОАО Тепличный")
     elif query_text == "ZLKR":
@@ -145,6 +145,62 @@ def get_listing_result(query_text):
         return "По данному запросу ничего не найдено. Убедитесь в правильности введенных данных!"
 
 
+def get_not_result(query_text):
+    if query_text == "KENB":
+        return get_listing("ОАО Оптима Банк")
+    elif query_text == "RSBK":
+        return get_listing("ОАО \"РСК Банк\"")
+    elif query_text == "MAIR":
+        return get_listing("ОАО \"Международный аэропорт Манас\"")
+    elif query_text == "FKDB":
+        return get_listing("ОАО \"ФинансКредитБанк КАБ\"")
+    elif query_text == "AYLB":
+        return get_listing("ОАО \"Айыл Банк\"")
+    elif query_text == "ELST":
+        return get_listing("ОАО Электрические станции")
+    elif query_text == "KELD":
+        return get_listing("ОАО Келдике")
+    elif query_text == "SELK":
+        return get_listing("ОАО Северэлектро")
+    elif query_text == "UCHK":
+        return get_listing("ОАО Учкун")
+    elif query_text == "KTEL":
+        return get_listing("ОАО Кыргызтелеком")
+    elif query_text == "KTTS":
+        return get_listing("ОАО Кыргыз Тоо-Таш")
+    elif query_text == "BTST":
+        return get_listing("ОАО Бишкектеплосеть")
+    elif query_text == "CHGS":
+        return get_listing("ОАО Чакан ГЭС")
+    elif query_text == "EHSUb":
+        return get_listing("ОсОО \"ИХСАН-ОРИКС\"")
+    elif query_text == "TEPL":
+        return get_listing("ОАО Тепличный")
+    elif query_text == "ZLKR":
+        return get_listing("ОАО Керемет Банк")
+    elif query_text == "AYUUb":
+        return get_listing("ОсОО Аю")
+    elif query_text == "PMTBb":
+        return get_listing("ОсОО Первая Металлобаза")
+    elif query_text == "KICBb":
+        return get_listing("ЗАО Кыргызский Инвестиционно-Кредитный Банк")
+    elif query_text == "KALT":
+        return get_listing("ОАО Кыргызалтын")
+    elif query_text == "SALFb":
+        return get_listing("ОАО Микрофинансовая компания «Салым Финанс»")
+    elif query_text == "MABNb":
+        return get_listing("ОАО Микрофинансовая компания АБН")
+    elif query_text == "KSDB":
+        return get_listing("ОАО Кыргызсуудолбоор")
+    elif query_text == "FRPR":
+        return get_listing("ОАО Микрокредитная компания Фонд развития предпринимательства")
+    elif query_text == "BMZD":
+        return get_listing("ОАО Бишкекский машиностроительный завод")
+    elif query_text == "MTURb":
+        return get_listing("ОАО Торговый дом Мин Туркун")
+    else:
+        return "Убедитесь в правильности введенных данных при подключении уведомления-> "+query_text+"!"
+
 
 def get_listing(listening):
     url = 'https://www.kse.kg/ru/Listing'
@@ -164,3 +220,4 @@ def get_listing(listening):
     for res in result_table:
         if res.startswith(listening):
             return res
+
